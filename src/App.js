f9880246c6b94bd38a494ddfe8ef4468f92e4f4e
vicente benavides <vicente.benavides0@gmail.com>
2021-11-04 01:54:27 -0500

    segundo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code>iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+          Edit <code>src/App.js</code>iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 
         </p>
         <a
